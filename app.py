5fc7af3d25c9d18c82dbee7754957fdc7ebf78bd
phuongvmodev <phuong.nguyen@vmodev.com>
2020-09-21 15:08:53 +0700

    - add logging and middleware

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from  concurrent import futures
+from concurrent import futures
 import time
 import grpc
 import logging
@@ -16,25 +16,28 @@ from src.controllers.user import UserController
 from src.controllers.auth import AuthController
 
 from utils.logger import *
+from middlewares.auth_interceptor import AuthInterceptor
+
 
 def grpc_server(port):
 
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10), interceptors=(AuthInterceptor(),))
     user_service.add_UserServicer_to_server(UserController(), server)
     auth_service.add_AuthServicer_to_server(AuthController(), server)
 
-    #create all table in database
+    # create all table in database
     db.create_all()
-    #init log
+    # init log
     create_timed_rotating_log('logs/logfile.log')
 
     server.add_insecure_port('[::]:{}'.format(port))
     server.start()
-    
+
     print("Listening on port {}..".format(port))
     logger.info("Listening on port {}..".format(port))
 
     server.wait_for_termination()
+
 
 if __name__ == '__main__':
     port = get_system_config()['port']
---
+++ middlewares/auth_interceptor.py
@@ -0,0 +1,25 @@
+import grpc
+
+_SIGNATURE_HEADER_KEY = 'authorization'
+
+class AuthInterceptor(grpc.ServerInterceptor):
+    def __init__(self):
+        def abort(ignored_request, context):
+            context.abort(grpc.StatusCode.UNAUTHENTICATED, 'Invalid authorization')
+        self._abortion = grpc.unary_unary_rpc_method_handler(abort)
+    def intercept_service(self, continuation, handler_call_details):
+        # Example HandlerCallDetails object:
+        #     _HandlerCallDetails(
+        #       method=u'/helloworld.Greeter/SayHello',
+        #       invocation_metadata=...)
+        method_name = handler_call_details.method.split('/')[-1]
+        print("MiddleWare Method Data=", method_name)
+        invocation_metadata = handler_call_details.invocation_metadata
+        print("MiddleWare Header Data=", invocation_metadata)
+       
+        return continuation(handler_call_details)
+        # expected_metadata = (_SIGNATURE_HEADER_KEY, method_name[::-1])
+        # if expected_metadata in handler_call_details.invocation_metadata:
+        #     return continuation(handler_call_details)
+        # else:
+        #     return self._abortion
