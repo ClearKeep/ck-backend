effc1b8f5a3475465cd288c20f2d0b8e564bbd25
phuongvmodev <phuong.nguyen@vmodev.com>
2021-01-27 13:40:52 +0700

    fix: push notification when new message and call

--- src/controllers/message.py
+++ src/controllers/message.py
@@ -1,17 +1,11 @@
 from protos import message_pb2
 from src.controllers.base import *
 from middlewares.permission import *
-from utils.logger import *
 from middlewares.request_logged import *
 from src.services.message import MessageService, client_message_queue
-from src.services.signal import SignalService
-from queue import Empty
-from src.models.user import User
 from src.models.signal_group_key import GroupClientKey
-from utils.const import DeviceType
-from kalyke.payload import PayloadAlert
 from src.services.notify_push import NotifyPushService
-from firebase_admin import messaging
+
 
 
 class MessageController(BaseController):
@@ -47,50 +41,29 @@ class MessageController(BaseController):
                 message=request.message
             )
 
-            ios_tokens = []
-            android_tokens = []
-
-            # put for other client
+            # push notification for other client
+            other_clients_in_group = []
             if client_id:
                 message_channel = "{}/message".format(client_id)
                 if message_channel in client_message_queue:
                     client_message_queue[message_channel].put(new_message)
                 else:
                     #push text notification for client
-                    client = User().get_client_id_with_push_token(client_id)
-                    for client_token in client.User.tokens:
-                        if client_token.device_type == DeviceType.android:
-                            android_tokens.append(client_token.push_token)
-                        elif client_token.device_type == DeviceType.ios:
-                            arr_token = client_token.push_token.split(',')
-                            ios_tokens.append(arr_token[-1])
-
+                    other_clients_in_group.append(client_id)
             else:
                 #push for other people in group
-                lst_client_in_groups = GroupClientKey().get_clients_in_group_with_push_token(group_id)
+                lst_client_in_groups = GroupClientKey().get_clients_in_group(group_id)
                 for client in lst_client_in_groups:
                     if client.User.id != request.fromClientId:
                         message_channel = "{}/message".format(client.User.id)
                         if message_channel in client_message_queue:
                             client_message_queue[message_channel].put(new_message)
                         else:
-                            for client_token in client.User.tokens:
-                                if client_token.device_type == DeviceType.android:
-                                    android_tokens.append(client_token.push_token)
-                                elif client_token.device_type == DeviceType.ios:
-                                    arr_token = client_token.push_token.split(',')
-                                    ios_tokens.append(arr_token[-1])
+                            other_clients_in_group.append(client.User.id)
 
-            push_service = NotifyPushService()
-            if len(android_tokens) > 0:
-                notification = messaging.Notification(
-                    title='',
-                    body='You have a new message',
-                )
-                push_service.android_text_notifications(android_tokens, notification)
-            if len(ios_tokens) > 0:
-                payload_alert = PayloadAlert(title="ClearKeep", body="You have a new message")
-                push_service.ios_text_notifications(ios_tokens, payload_alert)
+            if len(other_clients_in_group) > 0:
+                push_service = NotifyPushService()
+                push_service.push_text_to_clients(other_clients_in_group, title="", body="You have a new message")
 
             return new_message
 
@@ -113,7 +86,7 @@ class MessageController(BaseController):
                     message_response = client_message_queue[message_channel].get()
                     if message_response is None:
                         break
-                    yield message_response #print(message_response)
+                    yield message_response
             except:
                 logger.info('Client {} is disconnected'.format(client_id))
                 context.cancel()
@@ -122,7 +95,9 @@ class MessageController(BaseController):
                 client_message_queue[message_channel] = None
                 del client_message_queue[message_channel]
                 print('len queue after=', len(client_message_queue))
-                #need push notify here
+                # push text notification for client
+                push_service = NotifyPushService()
+                push_service.push_text_to_clients(client_id, title="", body="You have a new message")
 
     @request_logged
     def Subscribe(self, request, context):
