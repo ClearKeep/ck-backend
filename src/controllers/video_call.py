73692c393eec63ac47e814575d1fd32b60f0babc
ductnvmo <duc.tran@vmodev.com>
2021-03-01 10:11:47 +0700

    feat: cancel_request_call

--- src/controllers/video_call.py
+++ src/controllers/video_call.py
@@ -26,3 +26,21 @@ class VideoCallController(BaseController):
             context.set_details(json.dumps(
                 errors, default=lambda x: x.__dict__))
             context.set_code(grpc.StatusCode.INTERNAL)
+
+    @request_logged
+    def cancel_request_call(self, request, context):
+        try:
+            header_data = dict(context.invocation_metadata())
+            introspect_token = KeyCloakUtils.introspect_token(header_data['access_token'])
+            from_client_id = introspect_token['sub']
+            group_id = request.group_id
+            client_id = request.client_id
+            obj_res = self.service.cancel_request_call(group_id, from_client_id, client_id)
+
+            return obj_res
+        except Exception as e:
+            logger.error(e)
+            errors = [Message.get_error_object(Message.CLIENT_CANCEL_REQUEST_CALL_FAILED)]
+            context.set_details(json.dumps(
+                errors, default=lambda x: x.__dict__))
+            context.set_code(grpc.StatusCode.INTERNAL)
