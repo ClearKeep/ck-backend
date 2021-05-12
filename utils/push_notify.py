c3396772c60a8c7d6b8a9c26975d622eed20ca0f
phuongvmodev <phuong.nguyen@vmodev.com>
2021-05-12 09:48:14 +0700

    fix: login google and notification

--- utils/push_notify.py
+++ utils/push_notify.py
@@ -25,7 +25,7 @@ client_ios_text = APNsClient(
 )
 
 
-async def ios_data_notification(registration_tokens, payload):
+async def ios_data_multi_notification(registration_tokens, payload):
     for token in registration_tokens:
         try:
             expiration = int(time.time()) + 10
@@ -34,16 +34,25 @@ async def ios_data_notification(registration_tokens, payload):
         except Exception as e:
             logger.error(e)
 
+async def ios_data_notification(registration_token, payload):
+    try:
+        expiration = int(time.time()) + 10
+        res_obj = await client_ios_voip._send_message(registration_token, payload, expiration=expiration)
+        logger.info("Push iOS data notify success with token: {}".format(registration_token))
+        logger.info(res_obj)
+    except Exception as e:
+        logger.error(e)
 
-async def ios_text_notifications(registration_tokens, payload):
-    alert = Payload(alert=payload, badge=1, sound="default")
-    for token in registration_tokens:
-        try:
-            expiration = int(time.time()) + 10
-            res = await client_ios_text._send_message(token, alert, expiration=expiration)
-            logger.info("Push iOS text notify success with token: {}".format(token))
-        except Exception as e:
-            logger.error(e)
+
+# async def ios_text_notifications(registration_tokens, payload):
+#     alert = Payload(alert=payload, badge=1, sound="default")
+#     for token in registration_tokens:
+#         try:
+#             expiration = int(time.time()) + 10
+#             res = await client_ios_text._send_message(token, alert, expiration=expiration)
+#             logger.info("Push iOS text notify success with token: {}".format(token))
+#         except Exception as e:
+#             logger.error(e)
 
 
 # init push service for Android
@@ -51,28 +60,28 @@ cred = credentials.Certificate(get_system_config()["device_android"].get("fire_b
 default_app = firebase_admin.initialize_app(cred)
 
 
-def android_text_notifications(registration_tokens, payload):
-    message = messaging.MulticastMessage(
-        tokens=registration_tokens,
-        notification=payload,
-        android=messaging.AndroidConfig(
-            priority="normal",
-            ttl=10
-        )
-    )
-    response = messaging.send_multicast(message)
-    logger.info('{0} messages were sent successfully'.format(response.success_count))
-    if response.failure_count > 0:
-        responses = response.responses
-        failed_tokens = []
-        for idx, resp in enumerate(responses):
-            if not resp.success:
-                # The order of responses corresponds to the order of the registration tokens.
-                failed_tokens.append(registration_tokens[idx])
-        logger.info('List of tokens that caused failures: {0}'.format(failed_tokens))
+# def android_text_notifications(registration_tokens, payload):
+#     message = messaging.MulticastMessage(
+#         tokens=registration_tokens,
+#         notification=payload,
+#         android=messaging.AndroidConfig(
+#             priority="normal",
+#             ttl=10
+#         )
+#     )
+#     response = messaging.send_multicast(message)
+#     logger.info('{0} messages were sent successfully'.format(response.success_count))
+#     if response.failure_count > 0:
+#         responses = response.responses
+#         failed_tokens = []
+#         for idx, resp in enumerate(responses):
+#             if not resp.success:
+#                 # The order of responses corresponds to the order of the registration tokens.
+#                 failed_tokens.append(registration_tokens[idx])
+#         logger.info('List of tokens that caused failures: {0}'.format(failed_tokens))
 
 
-def android_data_notification(registration_tokens, payload):
+def android_data_multi_notification(registration_tokens, payload):
     message = messaging.MulticastMessage(
         tokens=registration_tokens,
         data=payload,
@@ -91,3 +100,17 @@ def android_data_notification(registration_tokens, payload):
                 # The order of responses corresponds to the order of the registration tokens.
                 failed_tokens.append(registration_tokens)
         logger.info('List of tokens that caused failures: {0}'.format(failed_tokens))
+
+def android_data_notification(registration_token, payload):
+    message = messaging.Message(
+        token=registration_token,
+        data=payload,
+        android=messaging.AndroidConfig(
+            priority="high",
+            ttl=10
+        )
+    )
+    response = messaging.send(message)
+    logger.info('Android data notification')
+    logger.info(response)
+
