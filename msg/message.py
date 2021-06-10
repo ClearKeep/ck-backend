9e943653350c23480acd1c2edf019974c16438da
Loi Nguyen Van <84302565+lloydngnn@users.noreply.github.com>
2021-06-10 08:38:14 +0700

    Feat:clk32-368:edit sent message:3

--- msg/message.py
+++ msg/message.py
@@ -57,6 +57,7 @@ class Message:
     CLIENT_MISS_CALL_FAILED = 1047
     CLIENT_DECLINE_CALL_FAILED = 1048
     CLIENT_END_CALL_FAILED = 1049
+    CLIENT_EDIT_MESSAGE_FAILED = 1050
 
     msg_dict = {
         UNAUTHENTICATED: "Authentication required",
@@ -104,6 +105,7 @@ class Message:
         CLIENT_MISS_CALL_FAILED: "Miss Call failed, Please try again",
         CLIENT_DECLINE_CALL_FAILED: "Decline Call failed, Please try again",
         CLIENT_END_CALL_FAILED: "End Call failed, Please try again",
+        CLIENT_EDIT_MESSAGE_FAILED: "Edit message failed. Please try again",
     }
 
     @staticmethod
