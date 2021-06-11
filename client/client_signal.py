fd019cd82bfb7ed705775d1d604c43eb629e1bfa
phuongvmodev <phuong.nguyen@vmodev.com>
2021-06-11 14:02:28 +0700

    fix: group message

--- client/client_signal.py
+++ client/client_signal.py
@@ -9,6 +9,10 @@ class ClientSignal:
     def __init__(self, workspace_domain):
         self.stub = self.grpc_stub(workspace_domain)
 
+    def grpc_stub(self, workspace_domain):
+        channel = grpc.insecure_channel(workspace_domain)
+        return signal_pb2_grpc.SignalKeyDistributionStub(channel)
+
     def get_user_signal_key(self, client_id, workspace_domain):
         try:
             request = signal_pb2.PeerGetClientKeyRequest(clientId=client_id, workspace_domain=workspace_domain)
@@ -25,6 +29,12 @@ class ClientSignal:
         except:
             return None
 
-    def grpc_stub(self, workspace_domain):
-        channel = grpc.insecure_channel(workspace_domain)
-        return signal_pb2_grpc.SignalKeyDistributionStub(channel)
+    def workspace_group_get_client_key(self, request):
+        try:
+            response = self.stub.WorkspaceGroupGetClientKey(request)
+            return response
+        except:
+            return None
+
+
+
