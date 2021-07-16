e3c64ffe1be3cad3c59f5b0bcda731da68f3374e
phuongvmodev <phuong.nguyen@vmodev.com>
2021-07-16 10:54:48 +0700

    fix: upload chunk file

--- src/services/upload_file.py
+++ src/services/upload_file.py
@@ -44,7 +44,9 @@ class UploadFileService(BaseService):
         file_hash = None
         file_name = None
         file_content_type = None
-        for request in request_iterator:
+
+        #async for r in stub.FindNode(helloworld_pb2.FindNodeRequest()):
+        async for request in request_iterator:
             m = hashlib.new('md5', request.file_data_block).hexdigest()
             if m != request.file_data_block_hash:
                 raise Exception(Message.UPLOAD_FILE_DATA_LOSS)
