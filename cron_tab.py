3e06b176a3682c7f4ca787a3d519afe52a9364cd
Tho Phan <thor.thophan@setel.com>
2021-12-16 17:32:53 +0700

    update lib for security pass

--- cron_tab.py
+++ cron_tab.py
@@ -6,11 +6,11 @@ from utils.logger import *
 def cron_tab_update_turn_server():
     try:
         # run in the first time
-        os.system('ENV=production python3 -m client.client_nts')
+        os.system('ENV=production /usr/bin/python3 -m client.client_nts')
         # set cronjob in next time
         cron = CronTab(user='ubuntu')
         cron.remove_all()
-        job = cron.new(command='cd /home/ubuntu/prod-clk328/ck-backend && ENV=production python3 -m client.client_nts')
+        job = cron.new(command='cd /home/ubuntu/prod-clk328/ck-backend && ENV=production /usr/bin/python3 -m client.client_nts')
         #job.day.every(1)
         job.setall('0 0 * * *')
         cron.write()
