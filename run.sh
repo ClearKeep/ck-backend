2cba5245f7bac9106285333fd69534d90c8c68e9
thanhpt1-vmo <thanhpt1@vmodev.com>
2022-04-17 18:15:11 +0700

    add multiple environment variables in to a shell script

--- run.sh
+++ run.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
-export PYTHONPATH=./
+#export PYTHONPATH=./
+#
+#ARG=$1
+#if [ ${#ARG} -gt 0 ]; then
+#    if [ $ARG == '-p' ] || [ $ARG == '-proto' ]; then
+#        sh proto/gen.sh
+#        echo "gen protobuf file success"
+#    else
+#        python app.py
+#    fi
+#else
+#    python app.py
+#fi
 
-ARG=$1
-if [ ${#ARG} -gt 0 ]; then
-    if [ $ARG == '-p' ] || [ $ARG == '-proto' ]; then
-        sh proto/gen.sh
-        echo "gen protobuf file success"
-    else
-        python app.py
-    fi
-else
-    python app.py
-fi
+
+echo "REMEMBER!!! generate protobuf files"
+export PYTHONPATH=./
+export PYTHONUNBUFFERED=1
+export ENV=development
+export GRPC_VERBOSITY=DEBUG
+export GRPC_TRACE=tcp,http
+
+
+python app_grpc.py
+python app_http.py
+
+
