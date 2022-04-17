ebca62e278ce522e3f72f162fc9a09383867d776
thanhpt1-vmo <thanhpt1@vmodev.com>
2022-04-17 20:04:22 +0700

    thanhpt1-vmo-docker-compose: 4

--- run_common.sh
+++ run_common.sh
@@ -5,6 +5,6 @@ echo "REMEMBER!!! generate protobuf files"
 echo "REMEMBER!!! generate protobuf files"
 export PYTHONPATH=./
 export PYTHONUNBUFFERED=1
-export ENV=development
+export ENV=thanhpt1-vmo_development
 export GRPC_VERBOSITY=DEBUG
 export GRPC_TRACE=tcp,http
