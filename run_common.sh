fe03d8d64c803fb0f90d3209152b2c2dacff7f4f
thanhpt1-vmo <thanhpt1@vmodev.com>
2022-04-18 14:03:54 +0700

    MAJOR: fix ENV

--- run_common.sh
+++ run_common.sh
@@ -5,6 +5,7 @@ echo "REMEMBER!!! generate protobuf files, python3 -m grpc_tools.protoc -I=. pro
 echo "REMEMBER!!! generate protobuf files, python3 -m grpc_tools.protoc -I=. protos/*.proto --python_out=. --grpc_python_out=."
 export PYTHONPATH=./
 export PYTHONUNBUFFERED=1
-export ENV=thanhpt1-vmo_development
+# export ENV=thanhpt1-vmo_development
+export ENV=thanhpt1-vmo_development_dev-server
 export GRPC_VERBOSITY=DEBUG
 export GRPC_TRACE=tcp,http
