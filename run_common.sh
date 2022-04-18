feb2d2048d00e79bd6638d6d5212020434f59d05
thanhpt1-vmo <thanhpt1@vmodev.com>
2022-04-18 13:59:34 +0700

    chore: reminder

--- run_common.sh
+++ run_common.sh
@@ -1,8 +1,8 @@
-echo "REMEMBER!!! generate protobuf files"
-echo "REMEMBER!!! generate protobuf files"
-echo "REMEMBER!!! generate protobuf files"
-echo "REMEMBER!!! generate protobuf files"
-echo "REMEMBER!!! generate protobuf files"
+echo "REMEMBER!!! generate protobuf files, python3 -m grpc_tools.protoc -I=. protos/*.proto --python_out=. --grpc_python_out=."
+echo "REMEMBER!!! generate protobuf files, python3 -m grpc_tools.protoc -I=. protos/*.proto --python_out=. --grpc_python_out=."
+echo "REMEMBER!!! generate protobuf files, python3 -m grpc_tools.protoc -I=. protos/*.proto --python_out=. --grpc_python_out=."
+echo "REMEMBER!!! generate protobuf files, python3 -m grpc_tools.protoc -I=. protos/*.proto --python_out=. --grpc_python_out=."
+echo "REMEMBER!!! generate protobuf files, python3 -m grpc_tools.protoc -I=. protos/*.proto --python_out=. --grpc_python_out=."
 export PYTHONPATH=./
 export PYTHONUNBUFFERED=1
 export ENV=thanhpt1-vmo_development
