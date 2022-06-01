1e57d0cce076e577739a403e16cb3375a0ffb2cb
hoatvvmodev <hoatv@vmodev.com>
2022-06-01 12:29:59 +0700

    fix log function

--- middlewares/request_logged.py
+++ middlewares/request_logged.py
@@ -1,8 +1,10 @@
 import logging
 import pprint
+from functools import wraps
 logger = logging.getLogger(__name__)
 
 def request_logged(func):
+    @wraps(func)
     async def deco(*args, **kwargs):
         try:
             # TODO: make more readable log, args,...
